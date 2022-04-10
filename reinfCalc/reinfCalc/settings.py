6084a3751452003b138302a6ecabb6f25b0051fc
Michal Kowal <michal.kowal.66@gmail.com>
2022-04-10 13:37:43 +0200

    Add media directory

--- reinfCalc/reinfCalc/settings.py
+++ reinfCalc/reinfCalc/settings.py
@@ -124,6 +124,12 @@ STATICFILES_DIRS = [
     BASE_DIR / STATIC_URL
 ]
 
+# Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
