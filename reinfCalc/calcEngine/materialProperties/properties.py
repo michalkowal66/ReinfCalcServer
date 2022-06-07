96dffcbd635c86567593fc7132070529d32985cc
Michal Kowal <michal.kowal.66@gmail.com>
2022-06-07 17:23:42 +0200

    Modify materialProperties module documentation

--- reinfCalc/calcEngine/materialProperties/properties.py
+++ reinfCalc/calcEngine/materialProperties/properties.py
@@ -8,12 +8,13 @@ class ExpClass(Enum):
     in accordance to PN-EN 1992-1-1:2008/NA: 2010
 
     Each attribute holds fixed number of keys that represent values of concrete properties:
-    
+
     - exp_class: exposure class,
     - concrete_class: minimum concrete class that may be used with given exposure class,
     - max_wc: maximum water/cement ration
     - min_cem: minimum content of cement [kg/m^3]
     - remarks: remarks about exposure class
+    - S1-6: minimum concrete cover in [mm] for selected Strcutre class
     """
     X0 = {"exp_class": "X0", "concrete_class": "C12/15", "max_wc": None, "min_cem": None,
           "remarks": "No corrosion risk", "S1": 10, "S2": 10, "S3": 10, "S4": 10, "S5": 15, "S6": 20}
@@ -32,14 +33,11 @@ class ExpClass(Enum):
     XD3 = {"exp_class": "XD3", "concrete_class": "C35/45", "max_wc": 0.45, "min_cem": 320,
            "remarks": "Corrosion due to chlorides hazard", "S1": 30, "S2": 35, "S3": 40, "S4": 45, "S5": 50, "S6": 55}
     XS1 = {"exp_class": "XS1", "concrete_class": "C30/37", "max_wc": 0.5, "min_cem": 300,
-           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 20, "S2": 25, "S3": 30, "S4": 35, "S5": 40,
-           "S6": 45}
+           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 20, "S2": 25, "S3": 30, "S4": 35, "S5": 40, "S6": 45}
     XS2 = {"exp_class": "XS2", "concrete_class": "C35/45", "max_wc": 0.45, "min_cem": 320,
-           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 25, "S2": 30, "S3": 35, "S4": 40, "S5": 45,
-           "S6": 50}
+           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 25, "S2": 30, "S3": 35, "S4": 40, "S5": 45, "S6": 50}
     XS3 = {"exp_class": "XS3", "concrete_class": "C35/45", "max_wc": 0.45, "min_cem": 340,
-           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 30, "S2": 35, "S3": 40, "S4": 45, "S5": 50,
-           "S6": 55}
+           "remarks": "Corrosion due to sea water chlorides hazard", "S1": 30, "S2": 35, "S3": 40, "S4": 45, "S5": 50, "S6": 55}
     XF1 = {"exp_class": "XF1", "concrete_class": "C30/37", "max_wc": 0.55, "min_cem": 300,
            "remarks": "Freeze and thaw hazard", "S1": None, "S2": None, "S3": None, "S4": None, "S5": None, "S6": None}
     XF2 = {"exp_class": "XF2", "concrete_class": "C25/30", "max_wc": 0.55, "min_cem": 300,
@@ -72,6 +70,7 @@ class ConcreteClass(Enum):
     - fctk_0.05: 5% fractile tensile strength [MPa],
     - fctk_0.95: 95% fractile tensile strength [MPa],
     - Ecm: Elastic modulus [GPa]
+    - area_coefficient: Coefficient for min. reinforcement area
      """
     C12_15 = {"concrete_class": "C12/15", "fck": 12, "fck_cube": 15, "fcm": 20, "fctm": 1.6, "fctk_0.05": 1.1,
               "fctk_0.95": 2.0, "Ecm": 27, "area_coefficient": 0.0013}
@@ -100,7 +99,7 @@ class RebarGrade(Enum):
     in accordance to PN-B-03264:2002
 
     Each attribute holds fixed number of keys that represent values of concrete properties:
-    
+
     - steel_grade: steel grade,
     - fyd: design yield strength of steel grade
     """
@@ -143,7 +142,8 @@ translate = {
     "Ecm": "Elastic modulus [GPa]",
     "steel_grade": "Steel grade",
     "fyd": "Design yield strength [MPa]",
-    "fyk": "Characteristic yield strength [MPa]"
+    "fyk": "Characteristic yield strength [MPa]",
+    "area_coefficient": "Area coefficient",
 }
 """dict: Dictionary with elaborated keys of property classes
 """
